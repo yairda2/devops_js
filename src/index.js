6fed7994d40a62169a96586399c1e94b7d41f63e
Yair_Davidoff <88335811+yairda2@users.noreply.github.com>
2023-06-05 08:58:19 +0300

    change every thing

--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+const mongoURL = 'mongodb://localhost:27017';
+const dbName = 'gradesDB';
 
-// Connect to MongoDB Atlas
-mongoose.connect('mongodb+srv://yair:yair@cluster0.ijthrbs.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
-// Define student schema and model
-const studentSchema = new mongoose.Schema({
-  name: String,
-  exam1: Number,
-  exam2: Number,
-  exam3: Number,
-});
-
-const Student = mongoose.model('Student', studentSchema);
-
-// Set up routes
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.post('/register', async (req, res) => {
-  try {
-    const { name, exam1, exam2, exam3 } = req.body;
-    const student = new Student({ name, exam1, exam2, exam3 });
-    await student.save();
-    res.status(200).json({ message: 'Registration successful' });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
+app.use(express.static('public'));
 
 app.get('/grades', async (req, res) => {
   try {
-    const students = await Student.find();
-    res.status(200).json({ students });
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    const client = await MongoClient.connect(mongoURL);
+    const db = client.db(dbName);
+    const collection = db.collection('grades');
+
+    const grades = await collection.find().toArray();
+    res.send(grades);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-// Start the server
+app.post('/grades', async (req, res) => {
+  const { grade1, grade2, grade3 } = req.body;
+  
+  try {
+    const client = await MongoClient.connect(mongoURL);
+    const db = client.db(dbName);
+    const collection = db.collection('grades');
+
+    await collection.insertOne({
+      grade1: parseInt(grade1),
+      grade2: parseInt(grade2),
+      grade3: parseInt(grade3)
+    });
+
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
